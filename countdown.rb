ca33485045843fcab41032568ed8148c45c06e1b
Craig <craigjope@gmail.com>
2021-10-09 18:27:33 -0600

    complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+require "pry"
 
-def countdown
+def countdown(number)
+    number = 10
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
+# if number == 0
+#     puts "HAPPY NEW YEAR!"
+# end
+end
+
+def countdown_with_sleep(number)
+    sleep(5)
+    countdown(number)
 end
